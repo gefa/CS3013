d12b6fc1a9bc3884996fa6eaed749bdef7a3f29e
gefa <sgvozdenovic@wpi.edu>
2015-02-25 15:39:01 -0500

    fixed 4 user functions

--- phase1.c
+++ phase1.c
@@ -30,13 +30,17 @@ struct page_table//page table must have 1000 entries
   {
     int physical_addr;    // index to ram, ssd, hd arrays
     int present;          // in which memory is the page
-  }ram,ssd,hd;
+    }ram,ssd,hd;
+  //int physical_addr;    // index to ram, ssd, hd arrays
   int modified;         // modified bit (int) is only used for RAM
   int referenced;       // reference bit (int) is only used for RAM
   int lock;		// lock for current user
   int history;		// count variable for LRU replacement algorithm
 
-}page_table_entry[HD_SIZE];//index to page_table_entry is vAddr
+};//index to page_table_entry is vAddr
+
+struct page_table page_table_entry[HD_SIZE];
+//struct page_table table_ram[RAM_SIZE],table_ssd[SSD_SIZE],table_hd[HD_SIZE];
 
 // these arrays hold page values
 int ram[RAM_SIZE];
@@ -48,8 +52,25 @@ int * accessIntPtr(vAddr address);
 void unlockMemory(vAddr address);
 void freeMemory(vAddr address);
 
+// page fault handler tries to swap a page in
+// if successful it returns a vAddr to that page
 void pageFaultHandler()
 {}
+
+vAddr evict_ram()
+{
+  int cnt = 0;
+  int i=0;
+  int rand_ram[25];
+
+  for(i=0;cnt<25;++i)
+    {
+      if(page_table_entry[i].ram.present==1)
+	rand_ram[cnt]=i;//remember the virtual address of a page
+    }
+  
+  return rand_ram[rand()%25];
+}
 
 vAddr allocateNewInt()
 {
@@ -61,7 +82,7 @@ vAddr allocateNewInt()
       printf("No available pages\n");
       return -1;
     }
-  if(ram_cnt<25)
+  if(ram_cnt<RAM_SIZE)
     {
       for(i=0;i<HD_SIZE;++i)
 	if(page_table_entry[i].valid==0)
@@ -70,16 +91,34 @@ vAddr allocateNewInt()
       page_table_entry[i].valid=1;//reserve this page
       
       page_table_entry[i].ram.present=1;//mark as present in ram
-	    
+      
       // map this page to ram
-      page_table_entry[i].ram.physical_addr = ram_cnt;
+      page_table_entry[i].ram.physical_addr = ram_cnt;//fix this, we need to search the page table, to find which indexes are free
+      //or be smart and have a linked list to keep track of the memory
+
       ram_cnt += 1;
+      page_cnt += 1;
       return i;//return virtual address
     }
   else
     {
-      //call page fault handler
-	    
+      // call page fault handler
+      // we have to evict a page from RAM
+      int vAddr_evict = evict_ram();
+      if(ssd_cnt<SSD_SIZE)
+	{//swap a page to ssd from ram
+
+	  for(i=0;i<HD_SIZE;++i)
+	    if(page_table_entry[i].valid==0)
+	      break;//we found an available page
+
+	  page_table_entry[vAddr_evict].ram.present = 0;
+	  page_table_entry[vAddr_evict].ssd.present = 1;
+	  page_table_entry[vAddr_evict].ssd.physical_addr = ssd_cnt;
+	  ssd_cnt++;
+	  page_cnt += 1;
+	  return i;//return virtual address	  
+	}
       // if successful return vAddr to user, else error
     }
 
@@ -187,7 +226,10 @@ void freeMemory(vAddr address)
  * what is virtal and what physical addr. in our case?
  * virtual: 0-999
  * 
- */ 
+ * can we only have 1 semaphre for page table and lock it during the function use
+ * can we free-deallocate or unlock a page that is not in RAM
+ * do we have to keep pages back in lower memory
+*/ 
 
 /* MAIN */
 
@@ -196,13 +238,21 @@ int main(int argc, char **argv) {
   page_cnt=ram_cnt=ssd_cnt=hd_cnt=0;
   
   int i=0;
+  
   // initialize all vAddresses
   for(i=0;i<HD_SIZE;++i)
     {
       page_table_entry[i].valid = -1;//not valid
       //page_table_entry[i].page_number = i;
       //page_table_entry[i].memory = -1;//no where in memory
-    }
-	
+      }
+  /*
+  for(i=0;i<RAM_SIZE;++i)
+    table_ram.valid[i] = -1;
+  for(i=0;i<SSD_SIZE;++i)
+    table_ssd.valid[i] = -1;
+  for(i=0;i<HD_SIZE;++i)
+    table_hd.valid[i] = -1; 
+  */	
   return EXIT_SUCCESS;
 }
